ba487cf14c455e3d85c6611a3b3d3ee9eb912a91
Michael Kufner <>
2022-10-05 08:38:10 +0200

    Introduce Next.js
    I don't like and also I don't want to spend time with setup TypeScript, React environment... And Next will make this easy

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,30 +1,41 @@
-import express, {Application, NextFunction, Request, Response} from 'express';
+import express, {NextFunction, Request, Response} from 'express';
 import {getEstatesFromApi} from "./estate/api/estatesApi";
 import {saveEstates} from "./estate/saveEstate";
+import next from "next";
 
-export const createServer = (port: number) => {
-    const app: Application = express()
+const dev = process.env.NODE_ENV !== "production";
+const port = 80;
 
-    app.get('/', (req: Request, res: Response) => {
-        res.send('Hello world!')
+const app = next({dev});
+const nextAppHandler = app.getRequestHandler();
+
+app.prepare()
+    .then(() => {
+        const server = express()
+
+        server.get('/', (req: Request, res: Response) => {
+            app.render(req, res, "/Homepage");
+        })
+
+        server.get('/api/crawl-estates/:limit', (req: Request, res: Response, next: NextFunction) => {
+            const limit = Number(req.params.limit);
+
+            if (isNaN(limit)) {
+                next();
+                return;
+            }
+
+            getEstatesFromApi(limit).then(async (estates) => {
+                await saveEstates(estates);
+                res.send("OK")
+            });
+        })
+
+        server.get("*", async (req, res) => {
+            nextAppHandler(req, res);
+        })
+
+        server.listen(port, function () {
+            console.log(`App is listening on port ${port} !`)
+        })
     })
-
-    app.get('/api/crawl-estates/:limit', (req: Request, res: Response, next: NextFunction) => {
-        const limit = Number(req.params.limit);
-
-        if (isNaN(limit)) {
-            next();
-            return;
-        }
-
-        getEstatesFromApi(limit).then(async (estates) => {
-            await saveEstates(estates);
-            res.send("OK")
-        });
-    })
-
-    app.listen(port, function () {
-        console.log(`App is listening on port ${port} !`)
-    })
-
-}
